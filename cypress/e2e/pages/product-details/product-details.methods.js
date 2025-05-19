249b59b695ff03fe33ed51593a6faee240ae161e
Brandon787300 <brandonstuard7@gmail.com>
2025-05-19 09:34:56 -0600

    Creacion de rarchivos correspondientes a Product Details page

---
+++ cypress/e2e/pages/product-details/product-details.methods.js
@@ -0,0 +1,7 @@
+import {ProductDetailsElements} from './product-details.elements';
+
+export class ProducDetailMethods{
+    static clickOnAddToCartButton(){
+        ProductDetailsElements.buttons.addToCart.click();
+    }
+}
