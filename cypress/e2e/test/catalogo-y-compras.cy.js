2ebe0f06962b59ea25fc24a92f734f2f5798950b
BSV0709 <brandonspam79@gmail.com>
2025-05-25 15:43:36 -0600

    Reduccion de tiempo de espera estatico

--- cypress/e2e/test/catalogo-y-compras.cy.js
+++ cypress/e2e/test/catalogo-y-compras.cy.js
@@ -158,7 +158,6 @@ describe(CommonPageData.testSuites.catalogoYCompras, ()=>{
         Logger.stepNumber(13)
         Logger.step('Verificar que se muestra un mensaje de confirmacion y se redirige al usuario a la página de inicio.')
         ThankYouForYourPurchaseMethods.verifyGreenCheckMarkIsDisplayed()
-        cy.wait(3000)
         ThankYouForYourPurchaseMethods.clickOnOkButton()
         HomeMethods.verifyHomePageisShown()
 
